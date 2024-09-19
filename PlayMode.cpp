dbb45704dc8f7df7e4a175bd289e37450c2fd5c3
symbolic233 <ymak@andrew.cmu.edu>
2024-09-19 12:52:08 -0400

    Minor clarification

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -325,7 +325,7 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 			glm::vec3(-aspect + 0.1f * H, -0.8 + 0.1f * H, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
-		lines.draw_text("Z to play your keys, X to play target",
+		lines.draw_text("Z to play your keys, X to play target, Enter to submit",
 			glm::vec3(-aspect + 0.1f * H, -0.95 + 0.1f * H, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
